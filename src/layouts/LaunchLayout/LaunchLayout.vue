22a855736d3aa89ee4747e9b9e87cff4beca2f10
lindelea <kgo@lindelin.org>
2021-10-20 16:37:02 +0900

    デザイン調整

--- src/layouts/LaunchLayout/LaunchLayout.vue
+++ src/layouts/LaunchLayout/LaunchLayout.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="launch_layout">
     <router-view></router-view>
+    <version />
   </div>
 </template>
 
 <script>
+import Version from "../../components/Version/Version";
 export default {
   name: "LaunchLayout",
+  components: {Version},
   created() {
     document.body.classList.add("launch_layout_body");
   }
