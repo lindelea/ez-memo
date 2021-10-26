90fc4022b5cd8a373dc1d2189e32a73de39931f6
lindelea <kgo@lindelin.org>
2021-10-26 17:33:47 +0900

    ベースデザイン調整

--- src/pages/Elements.vue
+++ src/pages/Elements.vue
@@ -3,6 +3,15 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
+          <h1>Elements</h1>
+          <h2>Elements</h2>
+          <h3>Elements</h3>
+          <h4>Elements</h4>
+          <h5>Elements</h5>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-12">
           <button type="button" class="btn btn-primary m-2">
             <div class="corner left_top"></div>
             <div class="corner left_bottom"></div>
@@ -31,6 +40,47 @@
             <div class="corner right_bottom"></div>
             Danger
           </button>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-6">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              <div class="corner left_top"></div>
+              <div class="corner left_bottom"></div>
+              <div class="corner right_top"></div>
+              <div class="corner right_bottom"></div>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-6">
+          <h2>
+            A EZMEMO UI PROJECT
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
+          </p>
         </div>
       </div>
     </div>
