55d5734844971e6468278c1f3651471ce783950d
lindelea <kgo@lindelin.org>
2021-11-11 17:01:09 +0900

    デザイン調整

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('@popperjs/core')
+require('bootstrap')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/pages/Elements.vue
+++ src/pages/Elements.vue
@@ -2,12 +2,37 @@
   <div class="elements">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-12">
+        <div class="col-6">
           <h1>Elements</h1>
           <h2>Elements</h2>
           <h3>Elements</h3>
           <h4>Elements</h4>
           <h5>Elements</h5>
+        </div>
+        <div class="col-6">
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="row mt-4">
